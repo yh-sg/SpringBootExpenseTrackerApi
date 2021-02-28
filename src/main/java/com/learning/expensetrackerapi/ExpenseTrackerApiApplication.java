c36bd85430eeef7bc5f6d6a717fd58e71d9a677d
yonghan <wangyh.92@gmail.com>
2021-02-28 16:08:45 +0800

    JWT Authentication

--- src/main/java/com/learning/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/learning/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -2,6 +2,10 @@ package com.learning.expensetrackerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.learning.expensetrackerapi.filters.AuthFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
@@ -9,5 +13,13 @@ public class ExpenseTrackerApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 }
--- src/main/java/com/learning/expensetrackerapi/filters/AuthFilter.java
+++ src/main/java/com/learning/expensetrackerapi/filters/AuthFilter.java
@@ -9,15 +9,43 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.GenericFilterBean;
+
+import com.learning.expensetrackerapi.Constant;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 public class AuthFilter extends GenericFilterBean{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
-	}
-	
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String authHeader = httpRequest.getHeader("Authorization");
+        if(authHeader != null) {
+            String[] authHeaderArr = authHeader.split("Bearer ");
+            if(authHeaderArr.length > 1 && authHeaderArr[1] != null) {
+                String token = authHeaderArr[1];
+                try {
+                    Claims claims = Jwts.parser().setSigningKey(Constant.API_SECRET_KEY)
+                            .parseClaimsJws(token).getBody();
+                    httpRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
+                }catch (Exception e) {
+                    httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "invalid/expired token");
+                    return;
+                }
+            } else {
+                httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be Bearer [token]");
+                return;
+            }
+        } else {
+            httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be provided");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
 }
